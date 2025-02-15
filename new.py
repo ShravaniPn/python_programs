cf464aea3a26d3bc5d6ac4a77d588aae04f5aead
ShravaniPn <shravaninaniwadekar460@gmail.com>
2025-02-15 19:44:31 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,48 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello world")
+Hello world
+>>> name="shravani"
+>>> print (f"Hello :{name}")
+Hello :shravani
+>>> var3=f"dear,{name}"
+>>> var3
+'dear,shravani'
+>>> print(r"raw string:{name}")
+raw string:{name}
+>>> print(r"/r/n/t/a")
+/r/n/t/a
+>>> print("\n hello world")
+
+ hello world
+>>> 
+>>> print("\t hello world")
+	 hello world
+>>> print("\a hello world")
+ hello world
+>>> //variable declaration
+SyntaxError: invalid syntax
+>>> var=1
+>>> var1=12
+>>> var_=22
+>>> var12123=12
+>>> v1="Shravani"
+>>> v2="Naniwadekar"
+>>> v1+v2
+'ShravaniNaniwadekar'
+>>> v1*3
+'ShravaniShravaniShravani'
+>>> v1[0]
+'S'
+>>> v1[1]
+'h'
+>>> v2[0:3]
+'Nan'
+>>> v2[1:4]
+'ani'
+>>> v2>v1
+False
+>>> "h"in v2
+False
+>>> "n"in v1
+True
